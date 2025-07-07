d9e5194751c35d8b60410ed2564be1fb2ec5d279
zyy <gt7591665@gmail.com>
2025-07-07 20:10:14 +0800

    add framework for ds_cnn inference

--- common/src/models/ds_cnn_stream_fe/ds_cnn.cc
+++ common/src/models/ds_cnn_stream_fe/ds_cnn.cc
@@ -17,11 +17,47 @@ static void ds_cnn_stream_fe_init(void) {
 }
 
 // TODO: Implement your design here
+static void do_init_zeros_input(void) 
+{
+    puts("Loading zeros input");
+    //tflite_load_model(
+}
 
+static void do_init_label0(void) 
+{
+
+}
+
+
+static void do_init_label1(void) 
+{
+
+}
+
+static void do_init_label6(void) 
+{
+
+}
+
+static void do_init_label8(void) 
+{
+
+}
+
+static void do_init_label11(void) 
+{
+
+}
 static struct Menu MENU = {
     "Tests for ds_cnn_stream_fe",
     "ds_cnn_stream_fe",
     {
+        MENU_ITEM('1', "Run with zeros input", do_init_zeros_input),
+        MENU_ITEM('2', "Run with label0", do_init_label0),
+        MENU_ITEM('3', "Run with label1", do_init_label1),
+        MENU_ITEM('4', "Run with label6", do_init_label6),
+        MENU_ITEM('5', "Run with label8", do_init_label8),
+        MENU_ITEM('6', "Run with label11", do_init_label11),
         MENU_END,
     },
 };
