751da32871c69900831533b845b8133e4b6322e7
zyy <gt7591665@gmail.com>
2025-11-10 15:04:52 +0800

    example_cfu_v is used for test

--- proj/example_cfu_v/src/proj_menu.cc
+++ proj/example_cfu_v/src/proj_menu.cc
@@ -24,17 +24,36 @@
 namespace {
 
 // Template Fn
+
 void do_hello_world(void) { puts("Hello, World!!!\n"); }
 
 // Test template instruction
+void do_grid_cfu_op0(void) {
+  puts("\nExercise CFU Op0\n");
+  printf("a   b-->");
+  for (int b = 0; b < 6; b++) {
+    printf("%8d", b);
+  }
+  puts("\n-------------------------------------------------------");
+  for (int a = 0; a < 6; a++) {
+    printf("%-8d", a);
+    for (int b = 0; b < 6; b++) {
+      int cfu = cfu_op0(0, a, b);
+      printf("%8d", cfu);
+    }
+    puts("");
+  }
+}
+
+// Test template instruction
 void do_exercise_cfu_op0(void) {
-  puts("\nExercise CFU Op0 aka ADD\n");
+  puts("\nExercise CFU Op0\n");
   int count = 0;
   for (int a = -0x71234567; a < 0x68000000; a += 0x10012345) {
     for (int b = -0x7edcba98; b < 0x68000000; b += 0x10770077) {
       int cfu = cfu_op0(0, a, b);
-      printf("a: %08x b:%08x a+b=%08x cfu= %08x\n", a, b, a + b, cfu);
-      if (cfu != a + b) {
+      printf("a: %08x b:%08x cfu=%08x\n", a, b, cfu);
+      if (cfu != a) {
         printf("\n***FAIL\n");
         return;
       }
@@ -49,10 +68,12 @@ struct Menu MENU = {
     "project",
     {
         MENU_ITEM('0', "exercise cfu op0", do_exercise_cfu_op0),
+        MENU_ITEM('g', "grid cfu op0", do_grid_cfu_op0),
         MENU_ITEM('h', "say Hello", do_hello_world),
         MENU_END,
     },
 };
+
 };  // anonymous namespace
 
 extern "C" void do_proj_menu() { menu_run(&MENU); }
